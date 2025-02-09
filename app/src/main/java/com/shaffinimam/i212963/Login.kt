65a046bc30f40c14f5008623da69a9fed46271df
shaffin <i212963@nu.edu.pk>
2025-02-09 21:32:49 +0500

    login/reg

--- app/src/main/java/com/shaffinimam/i212963/Login.kt
+++ app/src/main/java/com/shaffinimam/i212963/Login.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,7 +18,11 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val regText = findViewById<TextView>(R.id.regt)
-
+        val login = findViewById<Button>(R.id.log)
+        login.setOnClickListener{
+            val intent = Intent(this,Navigation::class.java)
+            startActivity(intent)
+        }
         // Set onClickListener
         regText.setOnClickListener {
             val intent = Intent(this, Register::class.java) // Replace with your class name
